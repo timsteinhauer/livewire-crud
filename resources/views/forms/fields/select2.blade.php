97c7b16ef9a23b7246f774ef3a8f7cacb402aad8
Tim Steinhauer <t.steinhauer@high-office.com>
2022-03-18 19:26:05 +0100

    select2 bugfix

--- resources/views/forms/fields/select2.blade.php
+++ resources/views/forms/fields/select2.blade.php
@@ -1,23 +1,22 @@
 
+@php( $select2Name = str_replace('.','', str_replace('_','', $keyPath)))
 <div class="input-group mb-3 {{ $config["class"] ?? "" }}" style="{{ $config["style"] ?? "" }}">
-    <input type="hidden"
-           id="select2-dropdown-{{ str_replace('.','-', $keyPath) }}-target"
-           wire:model.debounce.500ms="{{ $keyPath }}">
+
+    <script wire:ignore>
+        var select2Options{{ $select2Name }} =
+            [ @foreach($config["options"] as $option)
+            {id: {!! $option["id"] ?? "''" !!}, text: "{{ $option["name"] }}"},
+                @endforeach ];
+    </script>
 
     <span class="input-group-text">{{ $title }}</span>
 
     <div wire:ignore class="flex-1 ts-lw-crud-select2-wrapper">
-
-        <select id="select2-dropdown-{{ str_replace('.','-', $keyPath) }}"
+        <select id="select2-dropdown-{{ $select2Name }}"
                 class="form-select @error($keyPath) is-invalid @enderror"
                 {{ isset($config["required"]) && $config["required"] ? "required" : "" }}
-                {{ isset($config["disabled"]) && $config["disabled"] ? "disabled" : "" }}
         >
-            @foreach($config["options"] as $option)
-                <option value="{{ $option["id"] }}" {{ $option["id"] == $value ? "selected" : "" }}>{!! $option["name"] !!}</option>
-            @endforeach
         </select>
-
     </div>
 
     @if( isset($config["prefix"]))
@@ -33,16 +32,48 @@
 
 @push('scripts')
     <script>
+        window.livewire.on('refresh{{ $select2Name }}', refresh => {
+            if( typeof select2{{ $select2Name }}Load == "function"){
+                console.log('call {{ $select2Name }}');
+                select2{{ $select2Name }}Load(refresh);
+            }
+        });
+
+        function select2{{ $select2Name }}Load(refresh = null){
+            if (jQuery('#select2-dropdown-{{ $select2Name }}').hasClass("select2-hidden-accessible")) {
+                // Select2 has been initialized
+                jQuery('#select2-dropdown-{{ $select2Name }}').select2('destroy');
+                jQuery('#select2-dropdown-{{ $select2Name }} option').remove();
+            }
+
+            if( refresh != null){
+                var json = JSON.parse(refresh);
+                var array = [];
+                for (var i=0;i<json.length;i++) {
+                    array.push({id: json[i]["id"], text: json[i]["name"]});
+                }
+                jQuery('#select2-dropdown-{{ $select2Name }}').select2({
+                    data: array,
+                });
+            }else{
+                jQuery('#select2-dropdown-{{ $select2Name }}').select2({
+                    data: select2Options{{ $select2Name }},
+                });
+            }
+        }
+
         jQuery(document).ready(function () {
-            jQuery('#select2-dropdown-{{ str_replace('.','-', $keyPath) }}').select2();
-            jQuery('#select2-dropdown-{{ str_replace('.','-', $keyPath) }}').on('change', function (e) {
-                var data = $('#select2-dropdown-{{ str_replace('.','-', $keyPath) }}').select2("val");
-                @this.set('{{ $keyPath }}', data);
+            select2{{ $select2Name }}Load();
+
+            jQuery('#select2-dropdown-{{ $select2Name }}').on('change', function (e) {
+                var data = $('#select2-dropdown-{{ $select2Name }}').select2("val");
+            @this.set('{{ $keyPath }}', data);
             });
-            jQuery('#select2-dropdown-{{ str_replace('.','-', $keyPath) }}').on('select2:open', function(e) {
-                jQuery('input.select2-search__field')
-                    .prop('placeholder', '{{ isset($config["placeholder"]) ? $config["placeholder"] : "Tippen um zu filtern..." }}');
+
+            $('#select2-dropdown-{{ $select2Name }}').on('select2:open', function(e) {
+                $('input.select2-search__field').prop('placeholder', '{{ isset($config["placeholder"]) ? $config["placeholder"] : "Tippen um zu filtern..." }}');
             });
         });
+
     </script>
 @endpush
